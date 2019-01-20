09ca5eddb7a6357998c095204ac2b707575dc45f
ylqi007 <yunlongqi@my.unt.edu>
2019-01-20 11:53:10 -0600

    20190120, read data from tfrecords

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SSD-TensorFlow_Qi_0.1
+
+## 1. Generate TFRecord files
+
+`tf_convert_data.py`                        <br />
+    |--> `pascalvoc_to_tfrecords.run()`     <br />
+        |--> `_add_to_tfrecord`             <br />
+                |--> `_process_image`       <br />
+                |--> `_convert_to_example`
+
+## 2. Get data from TFRecord files
+dataset = `dataset_factory.get()`                           <br />
+            |--> `pascalvoc_2007.get_split()`               <br />
+                    |--> `pascalvoc_common.get_split()`     <br />
+                            |--> return `slim.dataset.Dataset`  <br />
+
--- datasets/pascalvoc_common.py
+++ datasets/pascalvoc_common.py
@@ -46,3 +46,67 @@ VOC_LABELS = {
     'tvmonitor': (20, 'Indoor'),
 }
 
+
+def get_split(split_name, dataset_dir, file_pattern, reader,
+              split_to_size, items_to_descriptions, num_classes):
+    """
+    Gets a dataset tuple with instructions for reading Pascal VOC dataset.
+
+    :param split_name: A train/test split name.
+    :param dataset_dir: The base directory of the dataset sources.
+    :param file_pattern: The file pattern to use when matching the dataset sources.
+        It is assumed that the pattern contains a `%s` string so that the split name
+        can be inserted.
+    :param reader: The TensorFlow reader type.
+    :return:
+        A `Dataset` namedtupe.
+    :raise:
+        ValueError: if `split_name` is not valid train/test split.
+    """
+    if split_name not in split_to_size:
+        raise ValueError('split name %s was not recognized.' % split_name)
+    file_pattern = os.path.join(dataset_dir, file_pattern % split_name)
+
+    # Allowing None in the signature so that dataset_factory can use the default.
+    if reader is None:
+        reader = tf.TFRecordReader
+    # Features in Pascal VOC TFRecords.
+    keys_to_features = {
+        'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
+        'image/format': tf.FixedLenFeature((), tf.string, default_value='jpeg'),
+        'image/height': tf.FixedLenFeature([1], tf.int64),
+        'image/width': tf.FixedLenFeature([1], tf.int64),
+        'image/channels': tf.FixedLenFeature([1], tf.int64),
+        'image/shape': tf.FixedLenFeature([3], tf.int64),
+        'image/object/bbox/xmin': tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymin': tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/xmax': tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/ymax': tf.VarLenFeature(dtype=tf.float32),
+        'image/object/bbox/label': tf.VarLenFeature(dtype=tf.int64),
+        'image/object/bbox/difficult': tf.VarLenFeature(dtype=tf.int64),
+        'image/object/bbox/truncated': tf.VarLenFeature(dtype=tf.int64)
+    }
+    items_to_handlers = {
+        'image': slim.tfexample_decoder.Image('image/encoded', 'image/format'),
+        'shape': slim.tfexample_decoder.Tensor('image/shape'),
+        'object/bbox': slim.tfexample_decoder.BoundingBox(
+            ['ymin', 'xmin', 'ymax', 'xmax'], 'image/object/bbox/label'),
+        'object/label': slim.tfexample_decoder.Tensor('image/object/bbox/label'),
+        'object/difficult': slim.tfexample_decoder.Tensor('image/object/bbox/difficult'),
+        'object/truncated': slim.tfexample_decoder.Tensor('image/object/bbox/truncated'),
+    }
+    decoder = slim.tfexample_decoder.TFExampleDecoder(
+        keys_to_features, items_to_handlers)
+
+    labels_to_names = None
+    # if dataset_utils.has_labels(dataset_dir):
+    #     labels_to_names = dataset_utils.read_label_file(dataset_dir)
+
+    return slim.dataset.Dataset(
+        data_sources=file_pattern,
+        reader=reader,
+        decoder=decoder,
+        num_samples=split_to_size[split_name],
+        items_to_descriptions=items_to_descriptions,
+        num_classes=num_classes,
+        labels_to_names=labels_to_names)
